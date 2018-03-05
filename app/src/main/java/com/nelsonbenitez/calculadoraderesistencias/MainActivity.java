0d6c91884914f5c148f5cb0bf4ca20fcf3410543
Nelson <maksilorak@gmail.com>
2018-03-05 02:16:04 -0500

    Con ic_launcher cambiado, y su distribución tambien cambió

--- app/src/main/java/com/nelsonbenitez/calculadoraderesistencias/MainActivity.java
+++ app/src/main/java/com/nelsonbenitez/calculadoraderesistencias/MainActivity.java
@@ -1,3 +1,19 @@
+/* Autor: Nelson Benitez
+ * Fecha: 26-Febrero_2018
+ * Laboratorio: Laboratorio 1 Programación dispositivos móviles
+ * Profesor: Edwin Cubillos
+ * Descripción: Programa que calcula el valor de una resistencia tomando como valores de
+ *              entrada los colores de la franjas que tiene la resistencia.  El color de las
+ *              franjas se puede cambiar al hacer click en cada banda o franja.
+ *
+ *              Al hacer click los colores van cambiando de acuerdo al código de colores establecido
+ *              en el estándar.  Una vez establecidas las cuatro con sus respectivos colores,
+ *              se puede calcular la resistencia respectiva.  Si no se cambian los colores que trae
+ *              por defecto, el resultado sera nulo.
+ *
+ * */
+
+
 package com.nelsonbenitez.calculadoraderesistencias;
 
 import android.graphics.Color;
@@ -7,10 +23,18 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Para contar la cantidad de veces que se hace click sobre una franja de color
     int cantidad_veces_click_franja_uno=0, veces_clik_franja_dos=0,
             veces_click_franja_tres=0,clicks_franja_cuatro=0;
+
+    //Para llevar el control de las cifras y el color
     String ColorBandaUno, cifraUno,cifraDos,tolerancia;
+
+    //Para almacenar el valor total de la respuesta, incluyendo la tolerancia
     String valorTotal;
+
+    //Para modificar los texview
     TextView Result,color_uno;
 
     @Override
@@ -25,6 +49,8 @@ public class MainActivity extends AppCompatActivity {
     public void Cambiar_color(View view) {
         if (cantidad_veces_click_franja_uno==9) {cantidad_veces_click_franja_uno=0;}
         cantidad_veces_click_franja_uno++;
+
+        //Se llama la funcion para validar el color de la banda uno
         ColorBandaUno=Colorea(cantidad_veces_click_franja_uno, ColorBandaUno);
         color_uno.setBackgroundColor(Color.parseColor(ColorBandaUno));
     }
@@ -91,6 +117,8 @@ public class MainActivity extends AppCompatActivity {
     public void Cambiar_franja_dos(View view) {
         if (veces_clik_franja_dos==10){veces_clik_franja_dos=0;}
         TextView color_uno= (TextView)findViewById(R.id.franja_dos);
+
+        //se valida el color de la segunda banda
         ColorBandaUno=Colorea(veces_clik_franja_dos, ColorBandaUno);
         color_uno.setBackgroundColor(Color.parseColor(ColorBandaUno));
         veces_clik_franja_dos++;
@@ -100,6 +128,8 @@ public class MainActivity extends AppCompatActivity {
         if (veces_click_franja_tres==7){veces_click_franja_tres=10;}
         else if(veces_click_franja_tres==12){veces_click_franja_tres=0;}
         TextView color_uno= (TextView)findViewById(R.id.franja_tres);
+
+        //se valida el color de la banda tres
         ColorBandaUno=Colorea(veces_click_franja_tres, ColorBandaUno);
         color_uno.setBackgroundColor(Color.parseColor(ColorBandaUno));
         veces_click_franja_tres++;
@@ -110,6 +140,8 @@ public class MainActivity extends AppCompatActivity {
         if (clicks_franja_cuatro==3){clicks_franja_cuatro=10;}
         else if(clicks_franja_cuatro==12){clicks_franja_cuatro=1;}
         TextView color_uno= (TextView)findViewById(R.id.franja_cuatro);
+
+        //validación de color franja cuatro
         ColorBandaUno=Colorea(clicks_franja_cuatro, ColorBandaUno);
         color_uno.setBackgroundColor(Color.parseColor(ColorBandaUno));
         //Log.d("Valor clicks cuatro",String.valueOf(clicks_franja_cuatro));
@@ -128,18 +160,18 @@ public class MainActivity extends AppCompatActivity {
         switch (clicks)
         {
             case 1:
-                tolerancia=" ±1%";
+                tolerancia=" ±1%"; //color café o marrón
                 break;
 
             case 2:
-                tolerancia=" ±2%";
+                tolerancia=" ±2%";  //color rojo
                 break;
 
             case 10:
-                tolerancia=" ±5%";
+                tolerancia=" ±5%"; //color dorado
                 break;
             case 11:
-                tolerancia=" ±10%";
+                tolerancia=" ±10%"; //color plateado
                 break;
         }
     }
@@ -150,30 +182,30 @@ public class MainActivity extends AppCompatActivity {
         switch (click_franja_tres)
         {
             case 1:
-                valorTotal=uno+dos;
+                valorTotal=uno+dos; //multiplicacion por uno
                 break;
 
             case 2:
-                valorTotal=uno+dos+"0";
+                valorTotal=uno+dos+"0"; //multiplacador 10
                 break;
 
             case 3:
-                valorTotal=uno+"."+dos+"k";
+                valorTotal=uno+"."+dos+"k"; //multiplacador 100
                 break;
             case 4:
-                valorTotal=uno+dos+"k";
+                valorTotal=uno+dos+"k";  //multiplacador 1000
                 break;
 
             case 5:
-                valorTotal=uno+dos+"0k";
+                valorTotal=uno+dos+"0k"; ////multiplacador 10000
                 break;
 
             case 6:
-                valorTotal=uno+"."+dos+"M";
+                valorTotal=uno+"."+dos+"M";  ////multiplacador 100000
                 break;
 
             case 7:
-                valorTotal=uno+dos+"M";
+                valorTotal=uno+dos+"M";  ////multiplacador 1000000
                 break;
 
             case 11:
